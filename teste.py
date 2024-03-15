e6e4a5aacb66a254c2a8f89da985a4bf07bd8b5c
Allan Siriani <allan@fatecpompeia.edu.br>
2024-03-15 16:07:27 -0300

    espiral

--- teste.py
+++ teste.py
@@ -1,19 +1,12 @@
+
 import turtle
-
-#desenhar um quadrado
-t = turtle.Turtle()
-t.forward(100)
-t.left(90)
-t.forward(100)
-t.left(90)
-t.forward(100)
-t.left(90)
-t.forward(100)
-t.left(90)
-
-#pause
+colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
+t = turtle.Pen()
+turtle.bgcolor('black')
+for x in range(360):
+    t.pencolor(colors[x%6])
+    t.width(x//100 + 1)
+    t.forward(x)
+    t.left(59)
 turtle.mainloop()
 
-
-
-print ('quero dizer oi!!')
