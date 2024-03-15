ff2ce0bcabdaebb4695d62400fe17cde4b437ddd
Allan Siriani <allan@fatecpompeia.edu.br>
2024-03-15 16:03:55 -0300

    desenho do quadrado

--- teste.py
+++ teste.py
@@ -1,1 +1,19 @@
+import turtle
+
+#desenhar um quadrado
+t = turtle.Turtle()
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+
+#pause
+turtle.mainloop()
+
+
+
 print ('quero dizer oi!!')
